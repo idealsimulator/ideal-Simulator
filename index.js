47617fdc4580b1c43c15b2df281ddc3a92c52247
idealsimulator <idealsimulator@gmail.com>
2024-09-20 12:13:18 +0530

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["👀 Watching Simulation Games"];
+const statusMessages = ["👀 Watching Simulation Games", "🌱 Developed by qtbread", "🎮 Playing Food Truck Chef"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
